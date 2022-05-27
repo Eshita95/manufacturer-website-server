9f77077202543977bcf73df9c8d96cdf0ec68571
eshita95 <eshita.islam95@gmail.com>
2022-05-27 17:26:15 +0600

    admin added

--- index.js
+++ index.js
@@ -92,6 +92,13 @@ async function run() {
             res.send({ result, token });
         })
 
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin });
+        })
+
         app.put('/user/admin/:email', async (req, res) => {
             const email = req.params.email;
             const filter = { email: email };
@@ -102,14 +109,6 @@ async function run() {
             res.send(result);
 
         })
-
-        app.get('/admin/:email', async (req, res) => {
-            const email = req.params.email;
-            const user = await userCollection.findOne({ email: email });
-            const isAdmin = user.role === 'admin';
-            res.send({ admin: isAdmin });
-        })
-
 
     }
     finally {
