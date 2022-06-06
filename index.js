8464d5e9663662931a5ff9b8acec5e3e20252390
eshita95 <eshita.islam95@gmail.com>
2022-06-06 14:37:36 +0600

    text added

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ async function run() {
             res.send(result);
         })
 
-        // user create database
+        // user database create
         app.get('/user', async (req, res) => {
             const totalUser = await userCollection.find().toArray();
             res.send(totalUser)
